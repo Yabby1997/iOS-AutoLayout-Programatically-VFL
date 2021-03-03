c2bbd3bbf66807f873cf0dd2772db8d3fa1cfbb4
seunghun yang <yabby1997@gmail.com>
2021-03-03 17:34:20 +0900

    애플아이콘, Swift레이블, Skip버튼에 대한 Constraint 적용

--- VFL/ViewController.swift
+++ VFL/ViewController.swift
@@ -18,12 +18,18 @@ class ViewController: UIViewController {
     let appLabel: UILabel = {
         let label = UILabel()
         label.text = "Swift"
+        label.font = .boldSystemFont(ofSize: 18)
+        label.textColor = .systemGray
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     let skipButton: UIButton = {
         let button = UIButton()
+        button.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
         button.setTitle("Skip", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
     
@@ -37,6 +43,7 @@ class ViewController: UIViewController {
         let label = UILabel()
         label.text = "Welcome to Swift"
         label.font = .boldSystemFont(ofSize: 18)
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
@@ -44,6 +51,7 @@ class ViewController: UIViewController {
         let label = UILabel()
         label.text = "Swift는 macOS, iOS, iPadOS, watchOS, tvOS앱을 개발하기 위한 최적의 언어입니다."
         label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
@@ -51,6 +59,7 @@ class ViewController: UIViewController {
         let pageControl = UIPageControl()
         pageControl.numberOfPages = 5
         pageControl.currentPage = 0
+        pageControl.translatesAutoresizingMaskIntoConstraints = false
         return pageControl
     }()
     
@@ -65,7 +74,48 @@ class ViewController: UIViewController {
         view.addSubview(summaryLabel)
         view.addSubview(pageControl)
         
-    }
+        swiftImageView.isHidden = true
+        welcomeLabel.isHidden = true
+        summaryLabel.isHidden = true
+        pageControl.isHidden = true
+        
+        let views: [String: Any] = [
+            "appleImageView": appleImageView,
+            "appLabel": appLabel,
+            "skipButton": skipButton
+        ]
 
+        var allConstraints: [NSLayoutConstraint] = []
+
+        let iconVerticalConstraints = NSLayoutConstraint.constraints(
+            withVisualFormat: "V:|-50-[appleImageView(30)]",
+            metrics: nil,
+            views: views
+        )
+        allConstraints += iconVerticalConstraints
+
+        let nameLabelVerticalConstraints = NSLayoutConstraint.constraints(
+            withVisualFormat: "V:|-55-[appLabel]",
+            metrics: nil,
+            views: views
+        )
+        allConstraints += nameLabelVerticalConstraints
+
+        let skipButtonVerticalConstraints = NSLayoutConstraint.constraints(
+            withVisualFormat: "V:|-50-[skipButton]",
+            metrics: nil,
+            views: views
+        )
+        allConstraints += skipButtonVerticalConstraints
+
+        let topRowHorizontalConstraints = NSLayoutConstraint.constraints(
+            withVisualFormat: "H:|-15-[appleImageView(30)]-[appLabel]-[skipButton]-15-|",
+            metrics: nil,
+            views: views
+        )
+        allConstraints += topRowHorizontalConstraints
+        
+        NSLayoutConstraint.activate(allConstraints)
+    }
 }
 
